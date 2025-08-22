f4652a76eb8d2aa671536310cfc17af0939adc52
wefi-dex <179937925+wefi-dex@users.noreply.github.com>
2025-08-21 21:44:10 -0500

    Update auth.ts

--- src/rest/controller/auth.ts
+++ src/rest/controller/auth.ts
@@ -28,7 +28,7 @@ export const login = asyncHandler(async (req: Request, res: Response) => {
     return res.status(401).json({ message: 'Invalid username or password.' }) as Response
   }
 
-  const secretKey = config.jwt.secret
+  const secretKey = config.defaultConfig.jwt.secret
   const options = { expiresIn: '1h' }
   const payload = { id: user.id, username: user.username }
   const token = jwt.sign(payload, secretKey, options)
@@ -38,3 +38,4 @@ export const login = asyncHandler(async (req: Request, res: Response) => {
   // avatar is now a string (URL or null)
   res.status(200).json({ token, user: userWithoutPassword }) as Response
 })
+
